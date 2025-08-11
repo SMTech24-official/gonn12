73f90521c8654a829a10e7ced7733af8f3bd32de
Md. Ashraful Alam Sifat <56783196+sifat2626@users.noreply.github.com>
2025-08-11 12:15:57 +0600

    Update environment variables and refactor email sending logic: correct Brevo email configuration and improve code formatting

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -1,6 +1,5 @@
-import nodemailer from 'nodemailer';
-import config from '../../config';
-
+import nodemailer from "nodemailer"
+import config from "../../config"
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
   try {
@@ -9,7 +8,7 @@ export const sendEmail = async (to: string, subject: string, html: string) => {
       port: 2525,
       secure: false, // Use TLS, `false` ensures STARTTLS
       auth: {
-        user: config.brevo_email, // Your email address
+        user: config.email, // Your email address
         pass: config.brevo_pass, // Your app-specific password
       },
     })
@@ -18,19 +17,19 @@ export const sendEmail = async (to: string, subject: string, html: string) => {
       from: `"Support Team" <${config.email}>`, // Sender's name and email
       to, // Recipient's email
       subject, // Email subject
-      text: html.replace(/<[^>]+>/g, ''), // Generate plain text version by stripping HTML tags
+      text: html.replace(/<[^>]+>/g, ""), // Generate plain text version by stripping HTML tags
       html, // HTML email body
-    };
+    }
 
     // Send the email
-    const info = await transporter.sendMail(mailOptions);
+    const info = await transporter.sendMail(mailOptions)
 
     // Log the success message
-    console.log(`Email sent: ${info.messageId}`);
-    return info.messageId;
+    console.log(`Email sent: ${info.messageId}`)
+    return info.messageId
   } catch (error) {
     // @ts-ignore
-    console.error(`Error sending email: ${error.message}`);
-    throw new Error('Failed to send email. Please try again later.');
+    console.error(`Error sending email: ${error.message}`)
+    throw new Error("Failed to send email. Please try again later.")
   }
-};
+}
